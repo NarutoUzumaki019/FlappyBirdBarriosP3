0fb67c3123867fcc368cdbe0a94487412659076c
NarutoUzumaki019 <723740@student.dusd.net>
2023-12-13 21:38:46 -0800

    I did stuff
    I did stuff

--- Flappy Bird/Assets/Scripts/Substitution Jutsu.cs
+++ Flappy Bird/Assets/Scripts/Substitution Jutsu.cs
@@ -8,20 +8,38 @@ public class SubstitutionJutsu : MonoBehaviour {
 
     private bool isDead = false;
     private Rigidbody2D rb2d;
+    private Animator anim;
 
-    // Start is called before the first frame update
+    // Use this for intialization
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (isDead == false)
         {
-            rb2d.velocity = Vector2.zero;
-            rb2d.AddForce(new Vector2(0, upforce));
+            if (Input.GetMouseButtonDown(0))
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upforce));
+                anim.SetTrigger("Flap");
+            }
         }
     }
+    void OnColissionEnter2D ()
+    {
+        isDead = true;
+        anim.SetTrigger ("Die");
+    }
 }
+   
+   
+   
+   
+   
+   
+
